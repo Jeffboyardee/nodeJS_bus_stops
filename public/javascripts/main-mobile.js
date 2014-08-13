20fd092bb9d8fea169ad4399ba7db8a8938371dd
Jeff Wei <jeff.wei@trailerpark.com>
2014-08-13 14:16:45 -0700

    Added loader gif and disabled drop downs until page is loaded

--- public/javascripts/main-mobile.js
+++ public/javascripts/main-mobile.js
@@ -19,16 +19,26 @@ $(function(){
 	$stopList = $('#stopList');
 	$predictions = $('#predictions');
 
+	// loading gif goes here //
+	$('#loading').show();
+
 	$.post('/mobile/agencySearch-mobile', function(data) {
 		$agencyList.html( optionsTemplate(data[0]) );
 		$routeList.html( routeTemplate(data[1]) );
 		$directionList.html( directionTemplate(data[2]) );	
 		$stopList.html( stopsTemplate(data[3]) );	
 		$predictions.html( predictionsTemplate(data[4]) );
+		// loading gif ends here //
+		$('#loading').hide();
 	});
 
 	$( "#agencyList" ).change(function() {
 		var parameters = { agency: $('#agencyList option:selected').attr('id') };
+		$('#routeList').attr('disabled', true);
+		$('#directionList').attr('disabled', true);
+		$('#stopList').attr('disabled', true);
+		// loading gif goes here //
+		$('#loading').show();
 
 		$.post('/mobile/agencySearchMobile-change-agency', parameters, function(data) {
 			console.log("success from server");
@@ -36,6 +46,11 @@ $(function(){
 			$directionList.html( directionTemplate(data[1]) );	
 			$stopList.html( stopsTemplate(data[2]) );	
 			$predictions.html( predictionsTemplate(data[3]) );
+			$('#routeList').attr('disabled', false);
+			$('#directionList').attr('disabled', false);
+			$('#stopList').attr('disabled', false);			
+			// loading gif ends here //
+			$('#loading').hide();
 		})
 		.fail(function() {
 			console.log("error from server");
@@ -47,12 +62,22 @@ $(function(){
 			agency: $('#agencyList option:selected').attr('id'), 
 			route: $('#routeList option:selected').attr('id') 
 		};
+		$('#agencyList').attr('disabled', true);
+		$('#directionList').attr('disabled', true);
+		$('#stopList').attr('disabled', true);
+		// loading gif goes here //
+		$('#loading').show();
 
 		$.post('/mobile/agencySearchMobile-change-route', parameters, function(data) {
 			console.log("success from server");
 			$directionList.html( directionTemplate(data[0]) );	
 			$stopList.html( stopsTemplate(data[1]) );	
 			$predictions.html( predictionsTemplate(data[2]) );
+			$('#agencyList').attr('disabled', false);
+			$('#directionList').attr('disabled', false);
+			$('#stopList').attr('disabled', false);		
+			// loading gif ends here //
+			$('#loading').hide();	
 		})
 		.fail(function() {
 			console.log("error from server");
@@ -65,11 +90,21 @@ $(function(){
 			route: $('#routeList option:selected').attr('id'),
 			direction: $('#directionList option:selected').attr('id') 
 		};
+		$('#agencyList').attr('disabled', true);
+		$('#routeList').attr('disabled', true);
+		$('#stopList').attr('disabled', true);
+		// loading gif goes here //
+		$('#loading').show();
 
 		$.post('/mobile/agencySearchMobile-change-direction', parameters, function(data) {
 			console.log("success from server");
 			$stopList.html( stopsTemplate(data[0]) );	
 			$predictions.html( predictionsTemplate(data[1]) );
+			$('#agencyList').attr('disabled', false);
+			$('#routeList').attr('disabled', false);
+			$('#stopList').attr('disabled', false);
+			// loading gif ends here //
+			$('#loading').hide();
 		})
 		.fail(function() {
 			console.log("error from server");
@@ -83,10 +118,20 @@ $(function(){
 			direction: $('#directionList option:selected').attr('id'),
 			stop: $('#stopList option:selected').attr('id')
 		};
+		$('#agencyList').attr('disabled', true);
+		$('#routeList').attr('disabled', true);
+		$('#directionList').attr('disabled', true);	
+		// loading gif goes here //
+		$('#loading').show();	
 
 		$.post('/mobile/agencySearchMobile-change-stop', parameters, function(data) {
 			console.log("success from server");
 			$predictions.html( predictionsTemplate(data[0]) );
+			$('#agencyList').attr('disabled', false);
+			$('#routeList').attr('disabled', false);
+			$('#directionList').attr('disabled', false);	
+			// loading gif ends here //
+			$('#loading').hide();	
 		})
 		.fail(function() {
 			console.log("error from server");
